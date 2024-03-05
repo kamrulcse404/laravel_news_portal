d56d4ecf4941d8eb64b32e22a48048a519db3f29
kamrulcse404 <kamrul.h01960@gmail.com>
2024-03-05 22:34:32 +0600

    add multi auth system part 1

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminAuthenticationController;
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('test', function(){
-    return 'Working';
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function(){
+    Route::get('login', [AdminAuthenticationController::class, 'login'])->name('login');
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('admin');
 });
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => ['admin']], function(){
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('admin');
+});
+
