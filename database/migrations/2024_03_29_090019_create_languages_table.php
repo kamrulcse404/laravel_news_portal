bd0980ff114c915743ef3fb227e2eefed4e079f6
kamrulcse404 <kamrul.h01960@gmail.com>
2024-03-29 16:31:26 +0600

    multi lang handling create form data part 1

--- database/migrations/2024_03_29_090019_create_languages_table.php
+++ database/migrations/2024_03_29_090019_create_languages_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('languages', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name');
+            $table->string('lang');
+            $table->string('slug');
+            $table->boolean('default');
+            $table->boolean('status');
+
             $table->timestamps();
         });
     }
--- resources/views/admin/language/create.blade.php
+++ resources/views/admin/language/create.blade.php
@@ -10,23 +10,33 @@
                 <h4>Create Language</h4>
             </div>
             <div class="card-body">
-                <form action="">
+                <form action="{{ route('admin.language.store') }}" method="POST">
+
+                    @csrf
+
                     <div class="form-group">
                         <label for="">Language</label>
-                        <select name="" id="" class="form-control">
+                        <select name="lang" id="language-select" class="form-control select2">
                             <option value=""> --select-- </option>
-                            <option value=""></option>
+                            @foreach (config('language') as $key => $lang)
+                                <option value="{{ $key }}">{{ $lang['name'] }}</option>
+                            @endforeach
                         </select>
                     </div>
 
                     <div class="form-group">
+                        <label for="">Name</label>
+                        <input readonly type="text" class="form-control" id="name" name="name">
+                    </div>
+
+                    <div class="form-group">
                         <label for="">Slug</label>
-                        <input readonly type="text" class="form-control">
+                        <input readonly type="text" class="form-control" id="slug" name="slug">
                     </div>
 
                     <div class="form-group">
                         <label for="">Is it default?</label>
-                        <select name="" id="" class="form-control">
+                        <select name="default" id="" class="form-control">
                             <option value="0">No</option>
                             <option value="1">Yes</option>
                         </select>
@@ -34,7 +44,7 @@
 
                     <div class="form-group">
                         <label for="">Status</label>
-                        <select name="" id="" class="form-control">
+                        <select name="status" id="language-select" class="form-control">
                             <option value="1">Active</option>
                             <option value="0">Inactive</option>
                         </select>
@@ -48,3 +58,16 @@
 
     </section>
 @endsection
+
+@push('scripts')
+    <script>
+        $(document).ready(function(){
+            $('#language-select').on('change', function(){
+                let value = $(this).val();
+                let name = $(this).children(':selected').text();
+                $('#slug').val(value);
+                $('#name').val(name);
+            })
+        });
+    </script>
+@endpush
